1c0bfb245024865a25153438fb51aa26f6bceede
Sterling <Sterling.Krone>
2022-05-02 08:01:01 -0600

    first try at putting a script together to log my bloodpress for the doctor

---
+++ bloodPressureLogger.py
@@ -0,0 +1,22 @@
+"""
+Script Name:            bloodPressureLogger.py
+Date Created:           May 01 2022
+Created by:             Sterling Krone
+Last Modified by:       May 01 2022
+Revision of Python:     3.7
+Description:            Python script that allows the user to enter Systolic and Diastolic Numbers
+Dependences:            csv, 
+"""
+import csv
+##################
+# Test Parameters / Setup
+##################
+##################
+# Functions
+##################
+
+############################## START TEST ##############################
+x = float(input('Enter an angle in Radians: '))
+degree_sign = u'\N{DEGREE SIGN}'
+result = x * 180 / pi
+print('angle in Degrees: {}{}'.format(math.floor(result), degree_sign))
